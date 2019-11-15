37317851da263c9a1d70da7c730247433ea6fe72
Oualid Bougzime <obougzim@utbm.fr>
2019-11-15 11:30:20 +0100

    actioncable ajout

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -1,16 +1,17 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit, EventEmitter } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Counter } from './counter';
 import { Observable, Subscription } from 'rxjs';
 import { ActionCableService, Channel } from 'angular2-actioncable';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class CounterService {
-  public initialValue = [49, 72, 48];
-  public positionCounter = [49, 72, 48];
+  //public initialValue = [12, 5, 7];
+  private counterUrl = 'https://lp4asgadot.herokuapp.com/counters/';
+  private countersUrl = 'https://lp4asgadot.herokuapp.com/counters.json';
+
   subscription: Subscription;
 
   private counterObservable: Map<number, Observable<Counter>> = new Map()
@@ -20,7 +21,7 @@ export class CounterService {
     const channel: Channel = this.cableService
     .cable('wss://lp4asgadot.herokuapp.com/cable')
     .channel('CountersChannel ', {});
-   
+
       // Subscribe to incoming messages
       this.subscription = channel.received().subscribe(message => {
           console.log(message)
@@ -28,22 +29,25 @@ export class CounterService {
   }
 
   reset() {
-    this.initialValue = [0, 0, 0];
+    //this.initialValue = [0, 0, 0];
   }
 
-  increment(position: number): number {
-    this.initialValue[position]++;
-    return this.initialValue[position];
+  increment(id: number): Observable<Counter> {
+    /*this.initialValue[position]++;
+    return this.initialValue[position];*/
+    return this.httpClient.patch<Counter>(this.counterUrl + id + '.json', {});
   }
 
   getCounter(id: number): Observable<Counter> {
     if (! this.counterObservable.has(id)) {
       this.counterObservable[id] = new EventEmitter<Counter>() 
     }
-    this.httpClient.get<Counter>("https://lp4asgadot.herokuapp.com/counters/"+this.positionCounter[id]+".json")
+    this.httpClient.get<Counter>("https://lp4asgadot.herokuapp.com/counters/"+ id +".json")
                    .subscribe(counter =>  this.counterObservable[id].emit(counter))
     return this.counterObservable[id]
   }
 
-
+  getCounters(): Observable<Counter[]> {
+    return this.httpClient.get<Counter[]>(this.countersUrl);
+  }
 }
